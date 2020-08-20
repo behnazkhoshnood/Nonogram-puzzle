cdd25a6ba2fec1a106bbe6858b4af45567fe7ae4
Behnaz Khoshnood <behnaz.khoshnood@gmail.com>
2020-08-20 15:04:26 +0000

    fixing the script rule for wining the game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,26 @@
- $(document).ready(function(){
- 
- function decrementHP() {
+
+$(document).ready(function () {
+        //                                                    winning rules
+        if ($(".not-clicked-color").length == 0){
+            alert("Congradulations. You have won the game");
+            $(".square").removeClass("square");
+        
+        setTimeout(function () {
+        location.reload(true);
+      }, 500);
+    }
+    
+    //                                                       loosing HP rules
+    function decrementHP() {
         var lives = document.getElementById("hp");
         var currentCount = lives.innerText;
 
         if (currentCount == 0) {
-          alert("You've lost this time!! Good luck for your next try");
-          location.reload()
+            alert("You've lost this time!! Good luck for your next try");
+            location.reload()
         } else {
 
-          lives.innerText = --currentCount;
+            lives.innerText = --currentCount;
         }
 
         if (currentCount == 2) {
@@ -19,44 +30,41 @@
         if (currentCount == 1) {
             lives.style.color = "red";
         }
-      }; 
+    };
+
+//                                                       on left click rules
+
+
+    $("div").on("click", function () {
+        $(this).removeClass("square").removeClass("not-clicked-color").addClass("clicked-color").unbind("click").unbind("contextmenu");
+    });
+
+    $(".gray").click(function() {
+        decrementHP();
+    });
+//                                                       on right click rules
+    $(".square").on("contextmenu", function () {
+        if ($(this).hasClass("gray")) {
+            $(this).removeClass("square").unbind("click").unbind("contextmenu");
+        }
+        else {
+            $(this).removeClass("square").removeClass("not-clicked-color").addClass("clicked-color").unbind("click").unbind("contextmenu");
+            decrementHP()
+        }
+    });
+// to remove the defult drop down menu when right click on squares on computers
+
+        $(".square").on("contextmenu", function () {
+            return false;
+        });
+        
+// to remove the defult drop down menu when right click on squares on mobile
+        $(".square").on("taphold", function () {
+            return false;
+        });
+});
+
+
+
+ 
       
-
-$("div").on("click", function () {
-    $(this).removeClass("square");
- });
- 
-$(".gray").on("click", function () {
-   decrementHP()
-});
- 
-$(".square").on("contextmenu", function(){
-    if($(this).hasClass("gray")){
-        $(this).removeClass("square");
-    }
-else{
-        decrementHP()
-    }
-});
-
-$(document).ready(function() {
-    $(".square").on("contextmenu",function(){
-       return false;
-    }); 
-}); 
-
-$(document).ready(function() {
-    $(".square").on("taphold",function(){
-       return false;
-    }); 
-});
-//  working on win alert
-function coloredSquares(){
-    var coloredSquares = $(".square").not(".gray");
-    if(coloredSquares == 0){
-        alert("congradulations, you win the game.")
-    }
-}
-
-});
-
