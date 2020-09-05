65ca235f221c31e48e9cf42c24e58172aee5577a
Behnaz Khoshnood <behnaz.khoshnood@gmail.com>
2020-09-05 05:19:35 +0000

    adding more style and code to ranking table

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,15 +1,12 @@
- function initMap() {
+function initMap() {
+    const myLatlng = {lat: 44.5200, lng: 36.3000};
     var map = new google.maps.Map(document.getElementById("map"), {
         zoom: 3,
-        center: {
-            lat: 44.5200,
-            lng: 36.3000
-        }
+        center: myLatlng
     });
-    
 
-     var labels = ["Alex", "Rohit", "Behnaz", "Garo", "Mahsa", "Eric", "Ali", "Behrooz", "Jack", "Fariba"];   
-    
+    var labels = ["Alex", "Rohit", "Behnaz", "Garo", "Mahsa", "Eric", "Ali", "Behrooz", "Jack", "Fariba"];
+
     var locations = [
         {lat: 52.5200, lng: 13.4050},//Berlin
         {lat: 51.5074, lng: 0.1278},//London
@@ -22,37 +19,57 @@
         {lat: 52.5200, lng: 13.4055},//Berlin
         {lat: 29.5922, lng: 52.5835} //Shiraz
     ];
-     
-    var markers = locations.map(function (location, i) { 
+
+    var markers = locations.map(function (location, i) {
         return new google.maps.Marker({
             position: location,
             label: labels[i % labels.length]
-        }); 
+        });
     });
-      
-	var previousMarker = null;
-	var namesTable = document.getElementById("names");
+
+    var previousMarker = null;
+    var namesTable = document.getElementById("names");
     var rows = namesTable.getElementsByTagName("tr");
     for (i = 1; i < rows.length; i++) {
-      var currentRow = namesTable.rows[i];
-      var createClickHandler = function(row) {
-        return function() {
-          var nameCell = row.getElementsByTagName("td")[1];
-          var name = nameCell.innerHTML;
-		  var marker = markers.find( function(m) {
-              return m.label==name;
-            });
-		  if (marker) {
-			  if (previousMarker && previousMarker.setMap) {
-                previousMarker.setMap(null);
-			  }
-			  marker.setMap(map);
-			  previousMarker = marker;
-		  }
+        var currentRow = namesTable.rows[i];
+        var createClickHandler = function (row) {
+            return function () {
+                var nameCell = row.getElementsByTagName("td")[1];
+                var name = nameCell.innerHTML;
+                var marker = markers.find(function (m) {
+                    return m.label == name;
+
+                });
+
+                if (marker) {
+                    if (previousMarker && previousMarker.setMap) {
+                        previousMarker.setMap(null)
+                    }
+                    marker.setMap(map);
+                    previousMarker = marker;
+                    map.setZoom(10);
+                    map.panTo(marker.position);
+                }
+            };
         };
-      };
-      currentRow.onclick = createClickHandler(currentRow);
-  }
+        currentRow.onclick = createClickHandler(currentRow);
+    }
 
 }
 
+$(document).ready(function () {
+    $("tr").click(function () {
+        $('tr').removeClass('make-purple');
+        $(this).addClass('make-purple');
+
+        function scrollToMap() {
+            var map = document.getElementById("map");
+            map.scrollIntoView();
+        }
+
+        $("tr").dblclick(function () {
+            scrollToMap();
+        });
+
+    });
+});
